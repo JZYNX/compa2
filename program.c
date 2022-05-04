411c122486d1cde5a6a2cba45cab2d91231cf50e
JZYNX <91903716+JZYNX@users.noreply.github.com>
2022-05-04 16:59:42 +1000

    Add files via upload

---
+++ program.c
@@ -0,0 +1,40 @@
+/* Restaurant recommender:
+ *
+ * Skeleton code written by Jianzhong Qi, April 2022
+ * Edited by: [Add your name and student ID here]
+ *
+ */
+
+#include <stdio.h>
+#include "listops.c"
+
+
+/* stage heading */
+#define HEADING "==========================Stage %d==========================\n"
+
+
+/* typedefs */
+
+
+/****************************************************************/
+/* function prototypes */
+void print_stage_header(int stage);
+
+
+/****************************************************************/
+
+/* main program controls all the action */
+int
+main(int argc, char *argv[]) {
+    /* TODO: Write your solution! */
+
+    return 0;
+}
+
+/* print stage header given stage number */
+void 
+print_stage_header(int stage_num) {
+	printf(HEADING, stage_num);
+}
+
+/* TODO: Define more functions! */
